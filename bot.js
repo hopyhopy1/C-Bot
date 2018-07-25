2296ecf190560cf6321c31a2954bd81203a67e12
hopyhopy1 <40838642+hopyhopy1@users.noreply.github.com>
2018-07-25 22:35:00 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.NDcwNzQ0MDE1MzIzNzkxMzgw.DjpdPw.pao9d2f5AKmWUeuGv8Ksxxp_A5k);

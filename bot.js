27da6e1ca0e5adab45458145bd556e1198f075d2
hopyhopy1 <40838642+hopyhopy1@users.noreply.github.com>
2018-07-25 23:20:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,37 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+var discord = require('discord.js');
+var client = new discord.Client();
+var token = process.env.BOT_TOKEN
+var prefix = proccess.env.PREFIX
+client.login(token)
 
-client.on('ready', () => {
-    console.log('I am ready!');
+client.on("ready", () => {
+  client.user.setGame(`Making HL3`);
+  console.log(`Ready to serve on ${client.guilds.size} servers, for ${client.users.size} users.`);
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+client.on('guildMemberAdd', member => {
+  let guild = member.guild;
+  let user = member.user
+  console.log(`${user.tag} joined ${guild}`)
 });
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.on('guildMemberRemove', member => {
+  let guild = member.guild;
+  let user = member.user
+  console.log(`${user.tag} left ${guild}`)
+});
+
+function isCommand(command, message){
+	var command = command.toLowerCase();
+	var content = message.content.toLowerCase();
+	return content.startsWith(prefix + command);
+}
+
+client.on('message', (message) => {
+	if (message.author.bot) return; // Dont answer yourself.
+    var args = message.content.split(/[ ]+/)
+    
+    if(isCommand('Ping', message)){
+    	message.reply('Pong');
+    }
+});
